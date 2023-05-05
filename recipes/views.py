02f52642fd58fbe487e40ebe03f8c6b49e1fc587
renatotnk <renato-tnk@live.com>
2023-05-05 17:37:37 -0300

    Moving templates & adding dynamic routing to views

--- recipes/views.py
+++ recipes/views.py
@@ -8,3 +8,9 @@ def home(req):
     return render(req, 'recipes/pages/home.html', status=200, context={
         'name': 'Test props'
     })
+
+
+def recipe(req, id):
+    return render(req, 'recipes/pages/recipe-view.html', status=200, context={
+        'name': 'Test props'
+    })
