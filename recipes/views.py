edc96da30cbdff0bb491306586a42e56dcdb64e7
renatotnk <renato-tnk@live.com>
2023-05-05 19:00:08 -0300

    Adding random test data to recipes

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,18 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
+
 # Create your views here.
 
 
 def home(req):
     return render(req, 'recipes/pages/home.html', status=200, context={
-        'name': 'Test props'
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 
 def recipe(req, id):
     return render(req, 'recipes/pages/recipe-view.html', status=200, context={
-        'name': 'Test props'
+        'recipe': make_recipe(),
     })
