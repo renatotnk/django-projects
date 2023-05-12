cb270d84858eac5ac3d811de0d84c48af8fede58
renatotnk <renato-tnk@live.com>
2023-05-12 14:35:58 -0300

    Importing recipes model and configuring static files path

--- recipes/models.py
+++ recipes/models.py
@@ -13,7 +13,7 @@ class Category(models.Model):
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
-    description = models.CharField(max_length=165)
+    description = models.CharField(max_length=665)
     slug = models.SlugField()
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
@@ -31,3 +31,6 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
     )
+
+    def __str__(self):
+        return self.title
