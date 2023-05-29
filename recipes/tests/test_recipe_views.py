26da0796fdbc756bc71eec194e13bcd05514cc31
renatotnk <renato-tnk@live.com>
2023-05-29 15:00:34 -0300

    Using Django requests to test HTTP response code

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -24,3 +24,11 @@ class RecipeViewsTest(TestCase):
             reverse('recipes:recipe', kwargs={'id': 1})
         )
         self.assertIs(view.func, views.recipe)
+
+    def test_recipe_view_returns_status_code_200_ok(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_view_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
