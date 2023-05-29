52d0deda41aa050b0f40ffb2063652150cdad966
renatotnk <renato-tnk@live.com>
2023-05-29 17:03:37 -0300

    Fixing servings field name in DB

--- recipes/tests/test_recipe_base.py
+++ recipes/tests/test_recipe_base.py
@@ -6,14 +6,8 @@ from recipes.models import Category, Recipe
 
 class RecipeTestBase(TestCase):
     def setUp(self) -> None:
-        category = Category.objects.create(name='Category')
-        author = User.objects.create_user(
-            first_name='user',
-            last_name='name',
-            username='username',
-            password='123456',
-            email='username@email.com',
-        )
+        category = self.make_category()
+        author = self.make_author()
         recipe = Recipe.objects.create(
             category=category,
             author=author,
@@ -22,10 +16,65 @@ class RecipeTestBase(TestCase):
             slug='recipe-slug',
             preparation_time='10',
             preparation_time_unit='Minutos',
-            servings_time=5,
-            servings_time_unit='Porções',
+            servings=5,
+            servings_unit='Porções',
             preparation_steps='Recipe Preparation Steps',
             preparation_steps_is_html=False,
             is_published=True,
         )
         return super().setUp()
+
+    def make_category(self, name='Categoria'):
+        return Category.objects.create(name=name)
+
+    def make_author(
+        self,
+        first_name='user',
+        last_name='name',
+        username='username',
+        password='123456',
+        email='username@email.com',
+    ):
+        return User.objects.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            password=password,
+            email=email,
+        )
+
+    def make_recipe(
+        self,
+        category_data=None,
+        author_data=None,
+        title='Recipe Title',
+        description='Recipe Description',
+        slug='recipe-slug',
+        preparation_time='10',
+        preparation_time_unit='Minutos',
+        servings=5,
+        servings_unit='Porções',
+        preparation_steps='Recipe Preparation Steps',
+        preparation_steps_is_html=False,
+        is_published=True,
+    ):
+        if category_data is None:
+            category_data = {}
+
+        if author_data is None:
+            author_data = {}
+
+        return Recipe.objects.create(
+            category=self.make_category(**category_data),
+            author=self.make_author(**category_data),
+            title=title,
+            description=description,
+            slug=slug,
+            preparation_time=preparation_time,
+            preparation_time_unit=preparation_time_unit,
+            servings_time=servings,
+            servings_time_unit=servings_unit,
+            preparation_steps=preparation_steps,
+            preparation_steps_is_html=preparation_steps_is_html,
+            is_published=is_published,
+        )
