fa585279fe46b4e21e18f3d72604f23fad93835c
renatotnk <renato-tnk@live.com>
2023-07-06 17:39:50 -0300

    Brief introduction to unittest mock & patch

--- recipes/tests/test_recipe_home_view.py
+++ recipes/tests/test_recipe_home_view.py
@@ -1,4 +1,5 @@
 # from unittest import skip
+from unittest.mock import patch
 
 from django.urls import resolve, reverse
 
@@ -56,3 +57,18 @@ class RecipeHomeViewTest(RecipeTestBase):
             '<h1>No Recipes found!</h1>',
             response.content.decode('utf-8')
         )
+
+    def test_recipe_home_is_paginated(self):
+        for i in range(8):
+            kwargs = {'slug': f'r{i}', 'author_data': {'username': f'u{i}'}}
+            self.make_recipe(**kwargs)
+
+        with patch('recipes.views.PER_PAGE', new=3):
+            response = self.client.get(reverse('recipes:home'))
+            recipes = response.context['recipes']
+            paginator = recipes.paginator
+
+            self.assertEqual(paginator.num_pages, 3)
+            self.assertEqual(len(paginator.get_page(1)), 3)
+            self.assertEqual(len(paginator.get_page(2)), 3)
+            self.assertEqual(len(paginator.get_page(3)), 2)
