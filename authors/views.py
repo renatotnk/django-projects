281cc6c22e9c9f478c4b9cc0ad742a298d4a74b9
renatotnk <renato-tnk@live.com>
2023-07-14 18:51:52 -0300

    Adding all form fields for better CSS control

--- authors/views.py
+++ authors/views.py
@@ -5,7 +5,11 @@ from .forms import RegisterForm
 
 # Create your views here.
 def register_view(req):
-    form = RegisterForm()
+    if req.POST:
+        form = RegisterForm(req.POST)
+    else:
+        form = RegisterForm()
+
     return render(req, 'authors/pages/register_view.html', {
         'form': form,
     })
